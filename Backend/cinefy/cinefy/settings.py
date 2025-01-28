da75c2ec70a910814ddddf20a0a0c5bf3b5339a5
Rahul Sanjeev <rahulsanjeev9@gmail.com>
2025-01-28 20:41:02 +0530

    Update build.sh, requirements.txt, and render.yaml

--- Backend/cinefy/cinefy/settings.py
+++ Backend/cinefy/cinefy/settings.py
@@ -52,8 +52,9 @@ MIDDLEWARE = [
     'django.middleware.common.CommonMiddleware',
 
     'django.middleware.security.SecurityMiddleware',
-    # WhiteNoiseMiddleware
-    "whitenoise.middleware.WhiteNoiseMiddleware",
+
+    "whitenoise.middleware.WhiteNoiseMiddleware",  # WhiteNoiseMiddleware
+
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -151,8 +152,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+if not DEBUG:
+    STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+    STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
