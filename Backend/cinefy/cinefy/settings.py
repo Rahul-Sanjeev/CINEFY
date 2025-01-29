2ee5ad0f9e90512896eccebc456d29c128e80052
Rahul Sanjeev <rahulsanjeev9@gmail.com>
2025-01-29 15:15:32 +0530

    Fix build command in render.yaml

--- Backend/cinefy/cinefy/settings.py
+++ Backend/cinefy/cinefy/settings.py
@@ -13,8 +13,8 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 import os
 from pathlib import Path
 
-from decouple import config
 import dj_database_url
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -30,7 +30,8 @@ SECRET_KEY = config('SECRET_KEY')
 DEBUG = False
 
 # Allow Vercel and Railway domains
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['https://cinefy-7oqz.onrender.com',
+                 'cinefy-7oqz.onrender.com']
 
 
 # Application definition
