16982baf1ce623a989c1a3914394a71240c583cf
Rahul Sanjeev <rahulsanjeev9@gmail.com>
2025-01-30 18:27:15 +0530

    Updated build.sh & render.yaml added

--- build.sh
+++ build.sh
@@ -5,10 +5,12 @@ set -o errexit
 # Install dependencies
 pip install -r Backend/cinefy/requirements.txt
 
+# Apply environment variables
+export DJANGO_SETTINGS_MODULE="cinefy.settings"
+export PYTHONPATH=$(pwd)/Backend/cinefy
 
 # Collect static files
 python Backend/cinefy/manage.py collectstatic --noinput
 
 # Run migrations
 python Backend/cinefy/manage.py migrate
-
