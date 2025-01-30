cdfef863bcde490aef901f4ca28ca681fcb4a0b9
Rahul Sanjeev <rahulsanjeev9@gmail.com>
2025-01-31 02:50:16 +0530

    build.sh updated

--- Backend/build.sh
+++ Backend/build.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 set -o errexit
 
+# Set the Django settings module
+export DJANGO_SETTINGS_MODULE="cinefy.settings"
+
+# Add Backend to the Python path
+export PYTHONPATH="${PYTHONPATH}:/opt/render/project/src/Backend"
+
 # Install dependencies
 pip install -r Backend/requirements.txt
 
@@ -9,6 +15,3 @@ python Backend/manage.py collectstatic --no-input
 
 # Run migrations
 python Backend/manage.py migrate
-
-# Add Backend to the Python path
-export PYTHONPATH="${PYTHONPATH}:/opt/render/project/src/Backend"
