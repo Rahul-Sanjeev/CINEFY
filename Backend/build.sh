2c040eccbddf790c5619d1554c4c3b06dd779c99
Rahul Sanjeev <rahulsanjeev9@gmail.com>
2025-01-31 04:12:35 +0530

    build.sh updated

--- Backend/build.sh
+++ Backend/build.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 set -o errexit
 
-# Add Backend directory to Python path
-export PYTHONPATH="${PYTHONPATH}:/opt/render/project/src/Backend"
+# Set the root project directory to Python path
+export PYTHONPATH="${PYTHONPATH}:/opt/render/project/src"
 
-# Install dependencies from Backend/requirements.txt
-pip install -r Backend/requirements.txt  # Adjust path if needed
+# Install dependencies
+pip install -r Backend/requirements.txt
 
 # Collect static files
 python Backend/manage.py collectstatic --no-input
