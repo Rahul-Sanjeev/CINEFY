bf457b945a73249d894f1353ac6f0a1255d9254f
Rahul Sanjeev <rahulsanjeev9@gmail.com>
2025-01-31 02:27:17 +0530

    deployment.py, settings.py updated

--- Backend/cinefy/settings.py
+++ Backend/cinefy/settings.py
@@ -156,7 +156,6 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
-STATIC_URL = BASE_DIR/'staticfiles'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
