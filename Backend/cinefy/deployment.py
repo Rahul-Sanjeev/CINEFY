ee4e408f8aada79446254dea11a4c58bc6655c3b
Rahul Sanjeev <rahulsanjeev9@gmail.com>
2025-01-31 01:34:27 +0530

    Project Restuctured & deployment process updated

---
+++ Backend/cinefy/deployment.py
@@ -0,0 +1,51 @@
+import os
+
+import dj_database_url
+
+from .settings import *
+from .settings import BASE_DIR
+
+ALLOWED_HOSTS = [os.environ.get["RENDER_EXTERNAL_HOSTNAME"]]
+
+
+STATIC_URL = BASE_DIR/'staticfiles'
+
+CSRF_TRUSTED_ORIGINS = ['https://'+os.environ.get["RENDER_EXTERNAL_HOSTNAME"]]
+
+DEBUG = False
+SECRET_KEY = os.environ.get["SECRET_KEY"]
+
+
+MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',  # This should be placed at the top
+    'django.middleware.common.CommonMiddleware',
+
+    'django.middleware.security.SecurityMiddleware',
+
+    "whitenoise.middleware.WhiteNoiseMiddleware",  # WhiteNoiseMiddleware
+
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+
+CORS_ALLOWED_ORIGINS = ['https://localhost:5173']
+
+STORAGES = {
+    'default': {
+        'BACKEND': 'django.core.file.storage.FileStorage',
+    },
+    'staticfiles': {
+        'BACKEND': 'whitenoise.storage.CompressedStaticFilesStorage',
+    }
+}
+
+DATABASES = {
+    'default': dj_database_url.config(
+        default=os.environ.get["DATABASE_URL"],
+        conn_max_age=600,  # Connection pool will close and reopen every 600 seconds
+    )
+}
