a51ec4641655cd468e3ae15f24b6687896ef584e
Rahul Sanjeev <rahulsanjeev9@gmail.com>
2025-02-01 01:39:09 +0530

    settings import updated

--- Backend/cinefy/deployment_settings.py
+++ Backend/cinefy/deployment_settings.py
@@ -1,9 +1,8 @@
 import os
 
 import dj_database_url
-from settings import *
 
-from .settings import BASE_DIR
+from .settings import *
 
 ALLOWED_HOSTS = [os.environ.get('RENDER_EXTERNAL_HOSTNAME')]
 CSRF_TRUSTED_ORIGIN = ['https://'+os.environ.get('RENDER_EXTERNAL_HOSTNAME')]
